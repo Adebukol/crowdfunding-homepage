6da9846ed9d90901eb3d18f3e2ca14fbe0c30840
Adebukol <adebukolaomowole@gmail.com>
2023-08-01 22:58:41 +0100

    mobile view overflow fixed

--- page.js
+++ page.js
@@ -1,13 +1,13 @@
 const projectButton = document.getElementById("projectBtn");
 const closeModal = document.querySelector(".close-modal");
-const openPledge = document.querySelector('radio');
+const openPledge = document.querySelector('.radio-btn');
 console.log(openPledge);
 projectButton.onclick = function(){
-  document.getElementById("modalPage").style.visibility = "visible";
+  document.getElementById("modalPage").style.display = "block";
 }
 
 closeModal.onclick =function(){
-    document.getElementById("modalPage").style.visibility = "hidden"; 
+    document.getElementById("modalPage").style.display = "none"; 
 }
 // openPledge.onclick= function(){
 //   console.log("checked")
@@ -16,6 +16,16 @@ closeModal.onclick =function(){
 //   document.querySelector('.modal').classList.toggle("active");
 // }
   function myFunction() {
-    var x = document.getElementById("modal").querySelectorAll(".radio-btn");
-    x[0].style.backgroundColor = "red";
+    const x = document.getElementById("modal").querySelectorAll(".radio-btn");
+    const pledge = document.querySelectorAll('.pledge');
+    pledge.style.display="block";
+    // x[0].style.backgroundColor = "red";
+
   }
+
+{/* <input type="radio" id="html" name="fav_language" value="HTML">
+ 
+<input type="radio" id="css" name="fav_language" value="CSS">
+<label for="css">CSS</label><br>
+<input type="radio" id="javascript" name="fav_language" value="JavaScript">
+<label for="javascript">JavaScript</label> */}
